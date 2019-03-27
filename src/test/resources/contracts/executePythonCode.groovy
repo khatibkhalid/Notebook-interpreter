fe0ea9fccb339c87eee122c5b8ca1a481887cd18
kkhatib <kkhatib@sqli-kkhatib.SQLI>
2019-03-27 20:36:24 +0100

    create test in groovy for executing simple python script

---
+++ src/test/resources/contracts/executePythonCode.groovy
@@ -0,0 +1,28 @@
+import com.github.tomakehurst.wiremock.http.Body
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make { 
+	description="Should execute python code snippet and return 2"
+	request{
+		method 'POST'
+		url '/interpreter/execute'
+		headers {
+			header('sessionId': 'khalid123')
+		  }
+		body("""
+		{
+			"code":"%python print(1+1)"
+		}
+			""")
+	}
+	response{
+		status 200
+		body("""
+		{
+			"result":"2"
+		}
+			""")
+ 
+	}
+}
