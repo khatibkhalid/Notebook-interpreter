d07dc5bb70c3ade30049116d0cf8c71b977594b7
Khalid_Khatib <khalid.khatib@sgs.com>
2019-03-27 11:29:21 +0100

    Create base test for interpreterService

---
+++ src/test/java/com/oracle/Notebook/service/InterpreterServiceTests.java
@@ -0,0 +1,34 @@
+package com.oracle.Notebook.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.oracle.notebook.exception.UnsupportedInterpreterException;
+import com.oracle.notebook.service.InterpreterService;
+import com.oracle.notebook.service.impl.InterpreterServiceImpl;
+import com.oracle.notebook.util.INTERPRETER;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= {InterpreterServiceImpl.class})
+public class InterpreterServiceTests {
+
+	@Autowired
+	private InterpreterService interpreterService;
+	
+	@Test
+	public void testBaseInterpreterService() throws UnsupportedInterpreterException, IOException {
+		assertEquals("2", interpreterService.executeCodeWithInterpreter("print(1+1)", INTERPRETER.PYTHON, null).getResult());
+	}
+
+	
+
+}
