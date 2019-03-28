f205816d374d26d996b6a97f39c7d6e72ad72c60
kkhatib <kkhatib@sqli-kkhatib.SQLI>
2019-03-28 21:11:14 +0100

    throw timeoutException and Log error

--- src/main/java/com/oracle/notebook/service/impl/InterpreterServiceImpl.java
+++ src/main/java/com/oracle/notebook/service/impl/InterpreterServiceImpl.java
@@ -5,7 +5,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.annotation.PreDestroy;
 
@@ -23,11 +26,14 @@ import com.oracle.notebook.util.INTERPRETER;
 @Service
 public class InterpreterServiceImpl implements InterpreterService {
 	
+	private static final int TIMEOUT_IN_MILLIS = 2000;
+	private static final String SCRIPTS_FOLDER = "scripts/";
 	private static final String SCRIPTS_FOLDER_NAME = "scripts";
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(InterpreterServiceImpl.class);
 
 	@Override
-	public InterpreterResult executeCodeWithInterpreter(String codeSnippet, INTERPRETER interpreter, String sessionId) throws UnsupportedInterpreterException, IOException {
+	public InterpreterResult executeCodeWithInterpreter(String codeSnippet, INTERPRETER interpreter, String sessionId) throws UnsupportedInterpreterException, IOException, TimeoutException {
 		switch (interpreter) {
 		case PYTHON:
 			return executeCodeWithPythonInterpreter(codeSnippet, sessionId);
@@ -37,35 +43,84 @@ public class InterpreterServiceImpl implements InterpreterService {
 	}
 
 	@Override
-	public InterpreterResult executeCodeWithPythonInterpreter(String codeSnippet, String sessionId) throws IOException {
+	public InterpreterResult executeCodeWithPythonInterpreter(String codeSnippet, String sessionId) throws IOException, TimeoutException {
 		LOGGER.info("Interpreter: {}, Code snippet: {}",INTERPRETER.PYTHON,codeSnippet);
+		
+		InterpreterResult interpreterResult = new InterpreterResult();
+		String result;
 		
 		File scriptsFolder = new File(SCRIPTS_FOLDER_NAME);
 		if(!scriptsFolder.exists()) scriptsFolder.mkdirs();
 		
 		String filePath;
 		if(sessionId!=null)
-			filePath = "scripts/script"+"_"+sessionId+".py";
+			filePath = SCRIPTS_FOLDER+"script"+"_"+sessionId+".py";
 		else
-			filePath = "scripts/script.py";
+			filePath = SCRIPTS_FOLDER+"script.py";
 		File f = new File(filePath);
 		
-		InterpreterResult interpreterResult = new InterpreterResult();
 		FileWriter fileWriter = new FileWriter(f, true);
 		BufferedWriter out = new BufferedWriter(fileWriter);
 		out.write(codeSnippet+"\n");
 		out.close();
+		
 		Process p = Runtime.getRuntime().exec("python "+filePath);
-		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String ret = in.readLine();
-		ret = ret == null ? "" : ret;
-		interpreterResult.setResult(ret);
-		f.deleteOnExit();
+		
+		try {
+			if(!p.waitFor(TIMEOUT_IN_MILLIS, TimeUnit.MILLISECONDS)) {
+				throw new TimeoutException("Execution timed out while waiting for the process to be executed !");
+			}
+		} catch (InterruptedException e) {
+			LOGGER.error("Error while waiting for executing Python process, error description: {}",e);
+			return interpreterResult;
+		}
+		
+		InputStream inputStream = p.getInputStream();
+		InputStream errorStream = p.getErrorStream();
+		
+		String inputStreamString = getStringFromInputStream(inputStream);
+		String errorStreamString = getStringFromInputStream(errorStream);
+
+		if(errorStreamString == null || "".equals(errorStreamString)) {
+			result = inputStreamString == null ? "" : inputStreamString;
+		} else {
+			result = errorStreamString;
+		}
+
+		interpreterResult.setResult(result);
 		return interpreterResult;
+	}
+	
+	private String getStringFromInputStream(InputStream is) {
+
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		String line;
+		try {
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+
+		} catch (IOException e) {
+			LOGGER.error("error while reading from inputStream: {}",e);
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return sb.toString();
+
 	}
 
 	@PreDestroy
-	public void removeFiles() throws IOException {
+	private void removeFiles() throws IOException {
 		File scriptsFolder = new File(SCRIPTS_FOLDER_NAME);
 				FileUtils.cleanDirectory(scriptsFolder);
 	}
